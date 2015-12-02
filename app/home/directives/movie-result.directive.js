addf43cc22549e14a825a2abf932caf66bc537f9
pearpages <pere@soms.cat>
2015-12-02 20:10:01 +0100

    directive movie-result tested

--- app/home/directives/movie-result.directive.js
+++ app/home/directives/movie-result.directive.js
@@ -14,7 +14,21 @@
 				scope:{
 					result: '='
 				},
-				template: '<div>{{vmd.result.Title}}</div>'
+				template: [
+				'<div class="row">',
+					'<div class="col-sm-4">',
+						'<img ng-src="{{vmd.result.Poster}}" alt="{{vmd.result.Title}}" width="220">',
+					'</div>',
+					'<div class="col-sm-8">',
+						'<h3>{{vmd.result.Title}}</h3>',
+						'<p>{{vmd.result.Plot}}</p>',
+						'<p><strong>Director:</strong> {{vmd.result.Director}}</p>',
+						'<p><strong>Actors:</strong> {{vmd.result.Actors}}</p>',
+						'<p><strong>Released:</strong> {{vmd.result.Released}}</p>',
+						'<p><strong>Genre:</strong> {{vmd.result.Genre}}</p>',
+					'</div>',
+				'</div>'
+				].join('')
 			};
 		
 			function controller() {
@@ -23,7 +37,7 @@
 				activate();
 		
 				function activate() {
-					console.log('directive');
+					//console.log('directive');
 				}
 			}
 	}
--- app/home/directives/movie-result.directive.spec.js
+++ app/home/directives/movie-result.directive.spec.js
@@ -6,9 +6,27 @@ describe('Movie Result Directive', function() {
 		Director: 'George Lucas',
 		Actors: 'Mark Hamill, Harrison Ford, Carrie Fisher, Peter Cushing',
 		Released: '25 May 1977',
-		Genre: 'Action, Adventure, Fantasy'
+		Genre: 'Action, Adventure, Fantasy',
+		Plot: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore ipsam fugit vero, autem asperiores accusamus ea unde magni, distinctio excepturi, minus amet repudiandae debitis nulla voluptatem earum deleniti error reiciendis culpa ut incidunt sunt. Omnis at minima non porro ullam hic delectus beatae eligendi rerum voluptatibus ratione tempora modi vitae architecto doloribus molestiae, nesciunt aperiam doloremque magni vero alias corporis. Ea cupiditate nostrum aliquid, repellendus, voluptate numquam expedita quod quasi. Dolore facere odit eaque quas dolorum, omnis illo minima mollitia minus ratione, alias, rem excepturi qui aliquid doloremque odio suscipit. Culpa laboriosam eveniet doloremque veniam quae sit quia unde, tempora!'
 	};
 	
+	//everywhere there's an ng-xxx attr or vmd.xxx ng-binding will be expected!!!
+	var expectedHtml = [
+		'<div class="row">',
+			'<div class="col-sm-4">',
+				'<img ng-src="http://localhost/image.jpg" alt="Star Wars: Episode IV - A New Hope" width="220" src="http://localhost/image.jpg">',
+			'</div>',
+			'<div class="col-sm-8">',
+				'<h3 class="ng-binding">Star Wars: Episode IV - A New Hope</h3>',
+				'<p class="ng-binding">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore ipsam fugit vero, autem asperiores accusamus ea unde magni, distinctio excepturi, minus amet repudiandae debitis nulla voluptatem earum deleniti error reiciendis culpa ut incidunt sunt. Omnis at minima non porro ullam hic delectus beatae eligendi rerum voluptatibus ratione tempora modi vitae architecto doloribus molestiae, nesciunt aperiam doloremque magni vero alias corporis. Ea cupiditate nostrum aliquid, repellendus, voluptate numquam expedita quod quasi. Dolore facere odit eaque quas dolorum, omnis illo minima mollitia minus ratione, alias, rem excepturi qui aliquid doloremque odio suscipit. Culpa laboriosam eveniet doloremque veniam quae sit quia unde, tempora!</p>',
+				'<p class="ng-binding"><strong>Director:</strong> George Lucas</p>',
+				'<p class="ng-binding"><strong>Actors:</strong> Mark Hamill, Harrison Ford, Carrie Fisher, Peter Cushing</p>',
+				'<p class="ng-binding"><strong>Released:</strong> 25 May 1977</p>',
+				'<p class="ng-binding"><strong>Genre:</strong> Action, Adventure, Fantasy</p>',
+			'</div>',
+		'</div>'
+	].join('');
+
 	var $compile;
 	var $rootScope;
 
@@ -22,8 +40,8 @@ describe('Movie Result Directive', function() {
 	it('should output movie result to expected HTML format', function() {
 		$rootScope.result = result;
 		var element;
-		element = $compile('<movie-result result="result"></movie-result>')($rootScope);
-		$rootScope.$digest(); //so we compile against the $rootScope!
-		expect(element.html()).toBe('<div class="ng-binding">Star Wars: Episode IV - A New Hope</div>');
+		element = $compile('<movie-result result="result"></movie-result>')($rootScope); //check $compile function and scope input param
+		$rootScope.$digest(); //so we compile the template against the $rootScope!
+		expect(element.html()).toBe(expectedHtml);
 	});
 })
